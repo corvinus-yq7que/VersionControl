635fdbfe514e89cbd30820e31f413cca5b83bb85
Benjámin Zsigovics <benjamin.zsigovics@stud.uni-corvinus.hu>
2024-01-08 16:47:59 +0100

    Adatok megjlenítése diagramon

--- webservice/webservice/Form1.Designer.cs
+++ webservice/webservice/Form1.Designer.cs
@@ -28,29 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dgwRates = new System.Windows.Forms.DataGridView();
+            this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dgwRates)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).BeginInit();
             this.SuspendLayout();
             // 
             // dgwRates
             // 
             this.dgwRates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgwRates.Location = new System.Drawing.Point(56, 51);
+            this.dgwRates.Location = new System.Drawing.Point(56, 117);
             this.dgwRates.Name = "dgwRates";
             this.dgwRates.RowHeadersWidth = 62;
             this.dgwRates.RowTemplate.Height = 28;
-            this.dgwRates.Size = new System.Drawing.Size(612, 342);
+            this.dgwRates.Size = new System.Drawing.Size(349, 433);
             this.dgwRates.TabIndex = 0;
+            // 
+            // chartRateData
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend1);
+            this.chartRateData.Location = new System.Drawing.Point(444, 117);
+            this.chartRateData.Name = "chartRateData";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartRateData.Series.Add(series1);
+            this.chartRateData.Size = new System.Drawing.Size(446, 433);
+            this.chartRateData.TabIndex = 1;
+            this.chartRateData.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(923, 605);
+            this.Controls.Add(this.chartRateData);
             this.Controls.Add(this.dgwRates);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgwRates)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +81,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgwRates;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRateData;
     }
 }
 
--- webservice/webservice/Form1.cs
+++ webservice/webservice/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using webservice.Entities;
 using webservice.MnbServiceReference;
@@ -23,6 +24,7 @@ namespace webservice
             InitializeComponent();
 
             dgwRates.DataSource = Rates;
+            chartRateData.DataSource = Rates;
 
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -35,7 +37,27 @@ namespace webservice
 
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
             XMLProcess(result);
+            DataDisplay();
+
+        }
+
+        private void DataDisplay()
+        {
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         private void XMLProcess(string result)
