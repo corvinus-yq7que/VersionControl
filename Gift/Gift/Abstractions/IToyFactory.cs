9826edfbe7a75a30c0a40dd8633159b333ca907c
Benjámin Zsigovics <benjamin.zsigovics@stud.uni-corvinus.hu>
2024-01-09 12:32:57 +0100

    Gyár absztrakció kezelése

---
+++ Gift/Gift/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using Gift.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gift.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
