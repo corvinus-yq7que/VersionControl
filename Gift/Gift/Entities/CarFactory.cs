eaabb0c6334ffd69060f793db6a5ff9cf3171529
Benjámin Zsigovics <benjamin.zsigovics@stud.uni-corvinus.hu>
2024-01-09 12:37:13 +0100

    Absztrakció felhasználása a gyárakban

--- Gift/Gift/Entities/CarFactory.cs
+++ Gift/Gift/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Gift.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
-        public Toy CreateNew()
-        {
-            return new Ball();
+        public Toy CreateNew() 
+        { 
+            return new Car(); 
         }
     }
 }
